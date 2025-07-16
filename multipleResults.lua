a58ec2f65e1d8eb61be13555ed9ff0d9fa0d0ed5
dotnetCarpenter <jon.ronnenberg@gmail.com>
2025-07-16 12:33:18 +0200

    multiple returns
    and destructoring assignment

---
+++ multipleResults.lua
@@ -0,0 +1,17 @@
+local function values()
+    return 3, 7
+end
+
+-- destructoring
+a, b = values()
+print(a)
+print(b)
+
+-- _ still has the value 3, so I guess this is convention
+_, c = values()
+print(c)
+
+-- print all return values
+print (values ())
+-- try to get length of return values (does not work)
+-- print (rawlen(values()))
